c0718f915609676ace733721efca846828f57edb
Patricia <akenahpatriciaapanyen@gmail.com>
2023-02-21 11:44:45 -0800

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,7 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-
-	for letter = ('a'; letter <= letter 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		for (letter = 'a'; letter <= letter 'z'; letter++)
+			_putchar(letter);
+		_putchar('/n');
 	}
-}
