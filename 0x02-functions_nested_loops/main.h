66be8f84acf9132a0fb8e94b69a7e805da03b193
Patricia <akenahpatriciaapanyen@gmail.com>
2023-02-22 07:33:11 -0800

    isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,12 +5,9 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-5-sign.c
-6-abs.c
-7-print_last_digit.c
-8-24_hours.c
-9-times_table.c
-10-add.c
-11-print_to_98.c
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
 
 #endif
