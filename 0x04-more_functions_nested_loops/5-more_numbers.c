f244684908de948a44021ba101a6045daf1acca6
Patricia <akenahpatriciaapanyen@gmail.com>
2023-02-23 22:09:52 -0800

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
@@ -19,3 +19,4 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
+
